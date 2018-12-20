d2156250f8f09dbd651d482eac32db747dd52969
1 <email>
2018-12-20 21:11:25 +0800

    init

---
+++ app/src/main/java/clwater/com/bleserialport/MainActivity.java
@@ -0,0 +1,13 @@
+package clwater.com.bleserialport;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
